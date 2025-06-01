de86344f1ccbf452ebe2b276d123213569455415
Valery Kotov <kotov.valery@gmail.com>
2025-06-01 21:36:36 +0200

    Style new poll component

--- src/components/NewPoll.tsx
+++ src/components/NewPoll.tsx
@@ -4,15 +4,19 @@ function NewPoll() {
   };
 
   return (
-    <div>
-      <h1>Create a New Poll</h1>
+    <div className="new-poll">
+      <h1>Would You Rather</h1>
+      <h2>Create Your Own Poll</h2>
       <form className="new-poll-form" onSubmit={onHandleSubmit}>
-        <textarea placeholder="Enter your question here" />
-        <label>Option 1</label>
-        <input type="text" placeholder="Option 1" />
-        <label>Option 2</label>
-        <input type="text" placeholder="Option 2" />
-        <button type="submit">Submit</button>
+        <label className="new-poll-label">Option 1</label>
+        <input className="new-poll-input" type="text" placeholder="Option 1" />
+
+        <label className="new-poll-label">Option 2</label>
+        <input className="new-poll-input" type="text" placeholder="Option 2" />
+
+        <button className="new-poll-submit" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
